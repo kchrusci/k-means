0ea5a7f7fa79b012b0430d5bed4c84ec2ab5c712
kchrusci <kchrusci@student.agh.edu.pl>
2015-12-08 21:00:06 +0100

    Modifications in histogram module and renaming file names in input directory

--- k_means/histogram.py
+++ k_means/histogram.py
@@ -2,24 +2,34 @@ import string
 
 
 class Histogram:
+
     def __init__(self, document):
         self.h = dict()
         self.fp = open(self.document)
         self.table = []
         self.document = document
+        self.word_flag = 0
+        self.doc_name = ""
+        self.words = []
+        self.numbers = []
 
-    def process_file(self):
+    def process_file(self, bad_words):
+        self.word_flag = 0
         for line in self.fp:
-            self.process_line(line, self.h)
+            self.process_line(line, bad_words)
         return self.h
 
-    @staticmethod
-    def process_line(line, h):
+    def process_line(self, line, bad_words):
         line = line.replace('-', ' ')
         for word in line.split():
+            word_flag = 0
             word = word.strip(string.punctuation + string.whitespace)
             word = word.lower()
-            h[word] = h.get(word, 0) + 1
+            for bad_word in bad_words:
+                if bad_word == word:
+                    word_flag = 1
+            if word_flag == 0:
+                self.h[word] = self.h.get(word, 0) + 1
 
     @staticmethod
     def total_words(h):
@@ -28,6 +38,11 @@ class Histogram:
     @staticmethod
     def different_words(h):
         return len(h)
+
+    def get_words(self):
+        for key, value in h.items():
+            self.words.append(key)
+            self.numbers.append(value)
 
     def most_common(self, h):
         for key, value in h.items():
