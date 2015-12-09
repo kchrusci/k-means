a5cf78a123406c3604c0a68f98f7380888863177
tfraczek <tfraczek@student.agh.edu.pl>
2015-12-09 11:12:49 -0800

    Modified first_centroids.py & presentation.py

--- k_means/first_centroids.py
+++ k_means/first_centroids.py
@@ -1,33 +1,34 @@
-# creating first centroids
+# creating first centroids from histograms with sorted (!) DATA
 
-most_common = []
+def create_first_centroids(number_of_groups, histograms) 
 
-# zalozenie: rozumiem ze pierwsza wartosc w tupli to numbers a druga words??
+    most_common = []  # create list of histograms best suited for being first centroids
 
-n = 9  # number of centroids to create
-
-for i in z:  # check all histograms
-    if len(most_common) < n:  
-        most_common.append(i)  # copy first histograms that will be soon replaced by better suited for the role of centroid 
-    else:
-        x, y = i.table[0]  # x - get the number of most commmon word used in this histogram
-        j = 0 
-        while j < n:  # go through out temporary "centroids" to check if this particular histogram is better suited than any temporary
-            a, b = most_common[j].table[0]  # 
-            if x > a:
-                most_common[(j+1):len(most_common)] = most_common[j:(len(most_common)-1)]
-                most_common[j] = i
-                break  # it is better so end loop and get the next histogram
-            else:
-                j += 1
-
-
-# now in most_common we have n histograms best suited for the role of centroid
-# let's delete all words but the most common in each of histogram  
+    for i in histograms:  # check all histograms
+        if len(most_common) < number_of_groups:  
+            most_common.append(i)  # copy first histograms that will be soon replaced by better suited for the role of centroid 
+        else:
+            number, word = i.table[0]  # get the number and the name of most common word used in this (already sorted) histogram
+            j = 0 
+            while j < number_of_groups:  # go through out temporary "centroids" 
+            # to check if this particular histogram is better suited than any temporary
+                temp_number, temp_word = most_common[j].table[0]  # 
+                if number > temp_number:
+                    most_common[(j+1):len(most_common)] = most_common[j:(len(most_common)-1)]  # shift the list
+                    most_common[j] = i  # and add the new temp histogram on appropriate position
+                    break  # it is placed on list so end loop and check the next histogram
+                else:
+                    j += 1
  
-for i in range(len(most_common)):
-    c = most_common[i]
-    x, y = c.table[0]
-    c.words = [y]
-    c.numbers = [x]
-    most_common[i] = c
+    # now in most_common we have number_of_groups histograms best suited for the role of centroid
+    # let's copy each histogram so that we don't change anything in it
+    # and delete all words but the most common in each copy
+
+    for i in range(len(most_common)):
+        copied_histogram = most_common[i]
+        number, value = copied_histogram.table[0]
+        copied_histogram.words = [y]
+        copied_histogram.numbers = [x]
+        most_common[i] = copied_histogram
+
+    return most_common
--- k_means/presentation.py
+++ k_means/presentation.py
@@ -1,9 +1,11 @@
-
 # presentation of DATA
 
-for i in groups:  # iterate as many times as many groups we have created
-    print "Group: "
-    for j in DATA:  # go through all probes
-        if j.group == i.number  # if this probe belongs to this group
-            print j.data.doc_name  # print the path to this probe's histogram -> we know to which group it belonged at the beginning
+def presentate(groups, DATA):
+    for i in groups:  # iterate as many times as many groups we have created
+        print "Group: "
+        for j in DATA:  # go through all probes
+            if j.group == i.number  # if this probe belongs to this group
+                print j.data.doc_name  # print the path to this probe's histogram 
+                # so that we know to which group it belonged to at the beginning
+    return None
     
